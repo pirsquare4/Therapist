3e2bf9c2dbe5c46e7857b14dd33efff7b64ea7f3
Teddy Tran <teddy.theodore.tran@gmail.com>
2019-04-07 16:55:58 +0200

    substitute

--- assignment1/Chatterbot.hs
+++ assignment1/Chatterbot.hs
@@ -105,9 +105,15 @@ reductionsApply _ = id
 
 -- Replaces a wildcard in a list with the list given as the third argument
 substitute :: Eq a => a -> [a] -> [a] -> [a]
-substitute _ _ _ = []
-{- TO BE WRITTEN -}
+substitute wildcard t s = concat [ replaceList x wildcard s| x <- t]
+--   where
+--     f x | x == wildcard = s 
+--         | otherwise     = x
 
+replaceList :: Eq a => a -> a -> [a] -> [a]
+replaceList b c d 
+  | b == c    = d
+  | otherwise = [b]
 
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
